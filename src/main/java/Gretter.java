c465264fdc467f8ff06a8702f6d30bdcce53a6ee
k-miyake <3katsu3hiro@gmail.com>
2014-06-22 09:15:31 +0900

    新規作成

--- src/main/java/Gretter.java
+++ src/main/java/Gretter.java
@@ -1,12 +1,25 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 
 public class Gretter {
-
+	private IAppDate appDate;
+	final Integer morningStart = 50000000;
+	final int dayTimeStart = 120000000;
+	final int eveningStart = 180000000;
+	
 	public Gretter(IAppDate appDate) {
-		// TODO Auto-generated constructor stub
+		this.appDate = appDate;
 	}
 
-	public String greet() {
-		return null;
+	public String greet() throws ParseException {
+		Integer time = Integer.parseInt(new SimpleDateFormat("HHmmssSSS").format(appDate.now()));
+		if(morningStart <= time && time < dayTimeStart){
+			return "おはよう";	
+		} else if(dayTimeStart <= time && time < eveningStart){
+			return "こんにちは";
+		}else{
+			return "こんばんは";
+		}
 	}
-
 }
--- src/test/java/GretterTest.java
+++ src/test/java/GretterTest.java
@@ -1,5 +1,5 @@
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -7,18 +7,61 @@ import java.util.Date;
 
 import org.junit.Test;
 
-
 public class GretterTest {
+	
+	@Test
+	public void 挨拶のテスト_5時丁度の場合_おはようを返す() throws ParseException {
+		doTestGreet("05:00:00.000", "おはよう");
+	}
 
 	@Test
-	public void 挨拶のテスト_5時丁度の場合_おはようを返す() {
+	public void 挨拶のテスト_5時1ミリ秒の場合_おはようを返す() throws ParseException {
+		doTestGreet("05:00:00.001", "おはよう");
+	}
+
+	@Test
+	public void 挨拶のテスト_12時1ミリ秒前の場合_おはようを返す() throws ParseException {
+		doTestGreet("11:59:59.999", "おはよう");
+	}
+
+	@Test
+	public void 挨拶のテスト_12時丁度の場合_こんにちはを返す() throws Exception {
+		doTestGreet("12:00:00.000", "こんにちは");
+	}
+
+	@Test
+	public void 挨拶のテスト_12時1ミリ秒の場合_こんにちはを返す() throws Exception {
+		doTestGreet("12:00:00.001", "こんにちは");
+	}
+
+	@Test
+	public void 挨拶のテスト_18時1ミリ秒前の場合_こんにちはを返す() throws Exception {
+		doTestGreet("17:59:59.999", "こんにちは");
+	}
+
+	@Test
+	public void 挨拶のテスト_18時丁度の場合_こんばんはを返す() throws Exception {
+		doTestGreet("18:00:00.000", "こんばんは");
+	}
+
+	@Test
+	public void 挨拶のテスト_18時1ミリ秒の場合_こんばんはを返す() throws Exception {
+		doTestGreet("18:00:00.001", "こんばんは");
+	}
+
+	@Test
+	public void 挨拶のテスト_5時1ミリ秒前の場合_こんばんはを返す() throws Exception {
+		doTestGreet("04:59:59.999", "こんばんは");
+	}
+
+	private void doTestGreet(final String time, String expectGreet) throws ParseException {
 		IAppDate appDate = new IAppDate() {
 			@Override
 			public Date now() throws ParseException {
-				return new SimpleDateFormat("HH:mm:ss.SSS").parse("05:00:00.000");
+				return new SimpleDateFormat("HH:mm:ss.SSS").parse(time);
 			}
 		};
 		String actual = new Gretter(appDate).greet();
-		assertThat(actual , is("おはよう"));
+		assertThat(actual , is(expectGreet));
 	}
 }
